70b4b09c06e28ec1053c86f289b32a92f900a4bd
Hugothms <hugothms@gmail.com>
2020-07-16 20:38:56 +0200

    up

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 20:30:57 by hthomas           #+#    #+#             */
+/*   Updated: 2020/07/16 20:37:02 by hthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libasm.h"
 #include <stdio.h>
 #include <string.h>
@@ -12,9 +24,12 @@ int	main()
 	printf("\n%ld\n%ld\n",	strlen("test"),	ft_strlen("test"));
 	
 	printf("\nstrcpy tests: (expected and mine)\n");
-	char *test = "test";
-	printf("%s\n%s\n", 		strcpy(test, ""),					ft_strcpy(test, ""));
-	printf("\n%s\n%s\n",	strcpy("hello world!", "test"),		ft_strcpy("hello world!", "test"));
+	char *hello = malloc(sizeof(char) * 14);
+	char *hi = malloc(sizeof(char) * 11);
+	hello = "Hello Wolrd!\0";
+	hi = "Hi Friend\0";
+	printf("%s\n%s\n", strcpy(hello, hi), ft_strcpy(hello, hi));
+	//printf("\n%s\n%s\n", strcpy("hello world!", "test"), ft_strcpy("hello world!", "test"));
 
 	return 0;
 }
