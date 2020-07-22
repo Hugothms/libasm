b34f06db718475ee60b3d0c7d80a36a37e199c40
Hugothms <hugothms@gmail.com>
2020-07-22 11:39:43 +0200

    main ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/16 20:30:57 by hthomas           #+#    #+#             */
-/*   Updated: 2020/07/16 20:37:02 by hthomas          ###   ########.fr       */
+/*   Updated: 2020/07/22 11:39:23 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,21 @@
 #include <stdio.h>
 #include <string.h>
 
-int	main()
+int main()
 {
 	printf("strlen tests: (expected and mine)\n");
-	printf("%ld\n%ld\n",	strlen(""),		ft_strlen(""));
-	printf("\n%ld\n%ld\n",	strlen("test"),	ft_strlen("test"));
-	printf("\n%ld\n%ld\n",	strlen("12\n3"),ft_strlen("12\n3"));
-	printf("\n%ld\n%ld\n",	strlen("test"),	ft_strlen("test"));
-	printf("\n%ld\n%ld\n",	strlen("test"),	ft_strlen("test"));
-	
+	printf("%ld\n%ld\n", strlen(""), ft_strlen(""));
+	printf("\n%ld\n%ld\n", strlen("test"), ft_strlen("test"));
+	printf("\n%ld\n%ld\n", strlen("12\n3"), ft_strlen("12\n3"));
+	printf("\n%ld\n%ld\n", strlen("test"), ft_strlen("test"));
+	printf("\n%ld\n%ld\n", strlen("test"), ft_strlen("test"));
+
 	printf("\nstrcpy tests: (expected and mine)\n");
-	char *hello = malloc(sizeof(char) * 14);
-	char *hi = malloc(sizeof(char) * 11);
-	hello = "Hello Wolrd!\0";
-	hi = "Hi Friend\0";
+	char hello[100] = "Hello Wolrd!\0";
+	char hi[100] = "Hi Friend\0";
 	printf("%s\n%s\n", strcpy(hello, hi), ft_strcpy(hello, hi));
-	//printf("\n%s\n%s\n", strcpy("hello world!", "test"), ft_strcpy("hello world!", "test"));
+	memcpy(hello ,"Hello Wolrd!\0", 14);
+	printf("\n%s\n%s\n", strcpy(hi, hello), ft_strcpy(hi, hello));
 
 	return 0;
 }
